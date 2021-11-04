be6df29ae0007766e2648468f542ce92871ce3d2
martin_frontroth <frontrothm@gmail.com>
2021-11-04 18:25:07 -0300

    avance en el 4

--- ejercicios.js
+++ ejercicios.js
@@ -36,10 +36,6 @@
 //     sumSquareDifference(numero)
 // );
 
-
-
-
-
 // Problem 2: Multiples of 3 and 5
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
@@ -108,41 +104,72 @@
 
 // Problem 4: Find the Symmetric Difference
 
-// The mathematical term symmetric difference (△ or ⊕) of two sets is the set
-// of elements which are in either of the two sets but not in both. For
-// example, for sets A = {1, 2, 3} and B = {2, 3, 4}, A △ B = {1, 4}.
-// Symmetric difference is a binary operation, which means it operates on
-// only two elements. So to evaluate an expression involving symmetric
-// differences among three elements (A △ B △ C), you must complete one
-// operation at a time. Thus, for sets A and B above, and C = {2, 3}, A △ B △ C
-// = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
+// The mathematical term symmetric difference (△ or ⊕) of two sets is the set of elements which are in either of the two sets but not in both. For
+// example, for sets A = {1, 2, 3} and B = {2, 3, 4}, A △ B = {1, 4}. Symmetric difference is a binary operation, which means it operates on
+// only two elements. So to evaluate an expression involving symmetric differences among three elements (A △ B △ C), you must complete one
+// operation at a time. Thus, for sets A and B above, and C = {2, 3}, A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
 
-// Create a function called sym that takes two or more arrays and returns
-// an array of their symmetric difference. The returned array must contain
+// Create a function called sym that takes two or more arrays and returns an array of their symmetric difference. The returned array must contain
 // only unique values (no duplicates).
 
 // sym([1, 2, 3], [5, 2, 1, 4]) should return [3, 4, 5].
-// sym([1, 2, 3], [5, 2, 1, 4]) should contain only three
-// elements.
+// sym([1, 2, 3], [5, 2, 1, 4]) should contain only three elements.
 // sym([1, 2, 3, 3], [5, 2, 1, 4]) should return [3, 4, 5].
 
-// sym([1, 2, 3, 3], [5, 2, 1, 4]) should contain only three
-// elements.
+// sym([1, 2, 3, 3], [5, 2, 1, 4]) should contain only three elements.
 // sym([1, 2, 3], [5, 2, 1, 4, 5]) should return [3, 4, 5].
-// sym([1, 2, 3], [5, 2, 1, 4, 5]) should contain only three
-// elements.
+// sym([1, 2, 3], [5, 2, 1, 4, 5]) should contain only three elements.
 // sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5]
-// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should contain only
-// three elements.
-// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1,
-// 4, 5].
-// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain
-// only three elements.
-// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should
-// return [2, 3, 4, 6, 7].
-// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should
-// contain only five elements.
-// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9,
-// 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
-// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9,
-// 8], [1]) should contain only eight elements.
+// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should contain only three elements.
+// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
+// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain only three elements.
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7].
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements.
+
+//Solución:
+
+// funcion va a recibir x arreglos, lo que se es que evaluarlos de a 2
+
+// el array no debria tener valores repetido, hay que borrarselos
+// deberia saber cuantos array llegan y contarlos
+// se deben trabajar de 2
+// no se la cantidad de elementos del array pero se que no seran vacios
+// debe haber una funcion que recorra el array y evalue con el onstoragedebe crearse el "nuevo" array para comparar con el que siga
+
+
+
+
+let array1 = [1, 2, 3];
+let array2 = [5, 2, 1, 4];
+
+let arrayFinal = [];
+
+console.log("array1", array1);
+console.log("array2", array2);
+
+
+function difSim(array1, array2) {
+  let arrayResultante = [];
+  arrayConcat = array1.concat(array2);
+  console.log("arrayConcat: ", arrayConcat);
+
+  for (i = 0; i < arrayConcat.length; i++) {
+    flag = true;
+
+    for (j = 0; j < arrayConcat.length; j++) {
+      if ((i !== j) && (arrayConcat[i] === arrayConcat[j])) {
+        flag = false;
+      }
+    }
+    if (flag) {
+      arrayResultante.push(arrayConcat[i]);
+    }
+  }
+return arrayResultante;
+}
+
+arrayFinal = difSim(array1,array2);
+console.log("arrayFinal", arrayFinal);
+
